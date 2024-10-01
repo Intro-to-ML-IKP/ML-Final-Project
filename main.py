bf59edab8d0134a8b521988a90611ca12590c009
Yppilf <109002969+Yppilf@users.noreply.github.com>
2024-10-01 15:44:11 +0200

    veryfast[push

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # Main program
+from stock_getter import Stock
+from matplotlib.pyplot import figure,show
+
+def main():
+    stock = Stock("AAPL", "2024-01-01", "2024-09-01")
+    closes = stock.get_data()
+    with open("test.txt", "w") as f:
+        for c in closes:
+            f.write(str(c))
+            f.write("\n")
+    
+    xList = range(len(closes))
+
+    fig = figure()
+    frame = fig.add_subplot()
+    frame.scatter(xList, closes)
+    show()
+
+if __name__ == "__main__":
+    main()
--- stock_getter.py
+++ stock_getter.py
@@ -1,22 +1,21 @@
 import yfinance as yf
 
-
 class Stock:
 
-    def __init__(self, name: str, StartDate: int, EndDate: int, interval :str = "1d"):
+    def __init__(self, name: str, startDate: str, endDate: str, interval: str = "1d"):
         """
         This Python function initializes an object with a name, start date, end date, and optional
         interval, downloads stock data using Yahoo Finance API, and extracts the closing prices.
         
         :param name: The name of the stock or financial
         instrument for which you want to download data
-        :param StartDate: The tart date for downloading stock data. 
+        :param StartDate: The start date for downloading stock data. 
         :param EndDate: The end date for the data you want to download.
         :param interval: The interval you want
         """
         self.name = name
-        self.start_date = StartDate 
-        self.end_date = EndDate
+        self.start_date = startDate 
+        self.end_date = endDate
 
         # Avoiding chosing interval of 1h
         if interval == "1h":
@@ -24,15 +23,15 @@ class Stock:
         else:
             self.interval = interval
 
-        self.Stock = yf.download(self.name, self.start_date, self.end_date, interval=self.interval)
+        self.stock = yf.download(self.name, self.start_date, self.end_date, interval=self.interval)
 
     def get_data(self):
         """f
         The method returns the closing stock prices from the `Stock` data.
         :return: The closing data
         """
-        StockData = self.Stock['Close']
-        return StockData
+        stockData = self.stock['Close']
+        return stockData
 
     def meanReturns_and_std(self):
         """
@@ -40,9 +39,9 @@ class Stock:
         :return: The function `meanReturns_and_std` is returning the mean returns and the standard
         deviation of the returns for the given interval.
         """
-        data = self.Stock['Close']
-        Returns = data.pct_change()
-        Standart_deviation = Returns.std()
-        meanReturns = Returns.mean()
-        return meanReturns, Standart_deviation
+        data = self.stock['Close']
+        returns = data.pct_change()
+        standard_deviation = returns.std()
+        meanReturns = returns.mean()
+        return meanReturns, standard_deviation
     
