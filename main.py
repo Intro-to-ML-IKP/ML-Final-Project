7edc8e159213fdc0aeced42681986e7d5162d2af
KTtheBugmaker2000 <katya.toncheva@gmail.com>
2025-01-08 16:13:34 +0100

    forcast rename

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from network.parameterConstructor import ParameterConstructor
 from network.network_constructor import NetworksConstructor, NetworksDict
 from results.result_handler import ResultsHandler
 from data_parser.dataFactory import StockDataFactory, DataReader
-from forcast.forecastFactory_initializer import ForcastFactoryInitializer
-from forcast.forcastFactory import ForcastFactory
+from forecast.forecastFactory_initializer import ForcastFactoryInitializer
+from forecast.forcastFactory import ForcastFactory
 from visualisation.visualize import PlotStocks
 
 
@@ -249,7 +249,7 @@ def forcast_closing_prices(
         the Mean Squared Error (MSE) between predictions and actual values.
     :rtype: tuple[list[float], float]
     """
-    # Create a forcast factory initializer
+    # Create a forecast factory initializer
     param_getter = ForcastFactoryInitializer()
 
     # Generate model parameters
@@ -271,14 +271,14 @@ def forcast_closing_prices(
         validation_percentage
         )
 
-    # Create a forcast factory
+    # Create a forecast factory
     forcaster = ForcastFactory(
         stock_name,
         model_parameters,
         datafactory_parameters
         )
 
-    # Make the forcast
+    # Make the forecast
     forcaster.predict(
         number_of_predictions,
         raw_data_amount,
@@ -288,7 +288,7 @@ def forcast_closing_prices(
         interval
         )
     
-    # Plot the forcast and the raw data
+    # Plot the forecast and the raw data
     forcaster.plot_predictions()
 
     # Compare the predictions with observations
