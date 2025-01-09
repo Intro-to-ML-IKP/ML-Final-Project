7af8e40f916183bcd842bb19d4128c8895d89994
KTtheBugmaker2000 <katya.toncheva@gmail.com>
2025-01-09 13:15:40 +0100

    lstm factory model networkfactory inheritance

--- trend_model/model_factory.py
+++ trend_model/model_factory.py
@@ -1,5 +1,25 @@
+from typing_extensions import override
+
+from network.networkFactory import NetworkFactory
+from trend_model.base_model import LstmModel
 
 
-class LSTM_Factory:
+class LSTMFactory(NetworkFactory):
 
-    # multiple lstm testing
+    @override
+    def __init__(
+            self,
+            model_shape: list[float],
+            activations: list[str],
+            input_shape: int,
+            output_shape: int = 1
+    ) -> None:
+        """
+        Instanitates a Network factory that is used to
+        construct a Neural Network.
+        """
+        super().__init__(model_shape,
+                         activations,
+                         input_shape,
+                         output_shape)
+        self._model = LstmModel()
