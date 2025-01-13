7433703c69959682982550857028039941415b1d
Petar <ppaureus@gmail.com>
2025-01-13 14:55:16 +0100

    Prep for testing

--- trend_model/test_run.py
+++ trend_model/test_run.py
@@ -1,7 +1,20 @@
 from trend_model import get_training_data
-from trend_model.model_factory import LSTMFactory
+from trend_model.base_model import LstmModel
 
 
-data = get_training_data()
+(
+    training_data,
+    validation_data,
+    testing_data,
+    training_labels,
+    validation_labels,
+    testing_labels
+    ) = get_training_data()
 
-idk = LSTMFactory() # needs params
+lstm = LstmModel() # needs params
+
+lstm.create_sequential_model()#(FILL SHIT IN THIS)
+
+lstm.compileModel(0.001, "mean square error", metrics=["mae"])
+
+lstm.trainModel(training_data, training_labels, validation_data, validation_labels, epochs=50, batch_size=5)
