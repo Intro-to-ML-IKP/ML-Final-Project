78e781fa81af816a3b25e132c15118b6f0c08f80
KTtheBugmaker2000 <katya.toncheva@gmail.com>
2025-01-15 16:29:57 +0100

    create_sequential_model edits

--- trend_model/base_model.py
+++ trend_model/base_model.py
@@ -1,3 +1,4 @@
+from keras.src.layers import ReLU
 from tensorflow.keras.layers import Dense # type: ignore
 from tensorflow.keras.layers import LSTM # type: ignore
 from tensorflow.keras.models import Sequential # type: ignore
@@ -26,18 +27,11 @@ class LstmModel(Model):
     """
     The base class for all models.
     """
-    #_model = None
-
-    '''def create_ARIMA_model(self, p, d, q, series):
-        self._model = ARIMA(series, p, d, q)'''
-
-    @override
     def create_sequential_model(
             self,
-            look_back: int,
-            model_shape: int,
-            # activations: list[str],
-            input_shape: list[int],
+            architecture: list[int],
+            activations: list[str],
+            input_shape: int,
             output_size: int
     ) -> None:
         """
@@ -52,21 +46,14 @@ class LstmModel(Model):
         :param output_size: Number of neurons in the output layer.
         :type output_size: int
         """
-        print(model_shape)
+        look_back = architecture[0]
+        model_shape = architecture[1]
+
+        print(input_shape)
         model = Sequential([
-            LSTM(units=model_shape, input_shape=(input_shape[1], look_back)),
+            LSTM(units=model_shape, input_shape=(input_shape, look_back)),
+            ReLU(),
             Dense(units=output_size)
         ])
 
-        # Defining LSTM model
-        # model.add(
-        #     LSTM(
-        #         model_shape, input_shape=(
-        #             input_shape[0], input_shape[1], look_back)))
-        # # output layer
-        # model.add(Dense(output_size))
-
         self.model = model
-
-    # def model(self) -> Sequential:
-    #     return self._model
