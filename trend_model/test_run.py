d2ae4ec2c50d7368ce8d5d78bbc98756f499a1f2
Iveto3 <i.i.ivanova@student.rug.nl>
2025-01-15 12:45:33 +0100

    test_run first working attempt

--- trend_model/test_run.py
+++ trend_model/test_run.py
@@ -13,19 +13,28 @@ def run():
         testing_labels
         ) = get_training_data()
 
+    print("training_data shape ",training_data.shape)
+    print("training_labels shape ",training_labels.shape[0])
     lstm = LstmModel()
+    
 
-    lstm.create_sequential_model(5, 10, training_data.shape, training_labels.shape[0])
+    lstm.create_sequential_model(9, 20, training_data.shape, training_labels.shape[0])
 
     lstm.compileModel(0.001, "mean_squared_error", metrics=["mae"])
 
-    scaled_training_data, scaled_validation_data = scale_data(training_data, validation_data)
-
+    scaled_training_data = scale_data(training_data)
+    scaled_validation_data = scale_data(validation_data)
+    
+    print("scaled_training_data shape ",scaled_training_data.shape)
+    print("scaled_validation_data shape ",scaled_validation_data.shape)
+    print("..................")
     x_train = reshape_input(scaled_training_data)
     x_val = reshape_input(scaled_validation_data)
+    print("x_train shape ",x_train.shape)
+    print("x_val shape ",x_val.shape)
+    print("validation_labels shape ",validation_labels.shape)
 
-    lstm.trainModel(x_train, training_labels, x_val, validation_labels,
-                    epochs=20, batch_size=15)
+    lstm.trainModel(x_train, training_labels, x_val, validation_labels, epochs=20, batch_size=15)
 
 
 def reshape_input(input_data) -> np.ndarray:
@@ -33,9 +42,10 @@ def reshape_input(input_data) -> np.ndarray:
     return x
 
 
-def scale_data(training_data, validation_data) -> tuple:
+def scale_data(data) -> tuple:
     # Scaling data
     min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
-    scaled_training_data = min_max_scaler.fit_transform(training_data.values.reshape(-1, 1))
-    scaled_validation_data = min_max_scaler.fit_transform(validation_data.values.reshape(-1, 1))
-    return scaled_training_data, scaled_validation_data
+    #for row in range(len(data)):
+    scaled_data = min_max_scaler.fit_transform(data) # .reshape(-1, 1)
+    # scaled_validation_data = min_max_scaler.fit_transform(validation_data.values.reshape(-1, 1))
+    return scaled_data
